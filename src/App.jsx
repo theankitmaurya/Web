8ba985a9b369d25ecb0f192d6087665d940d1e4f
The-Ankit-Maurya <ankitmaurya20223@gmail.com>
2024-07-25 19:31:24 +0530

    Added notifications folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import "./App.css";
+import Auth from "./pages/Auth/Auth";
 import Home from "./pages/Home/Home";
 import Profile from "./components/HomeUI/Profile/Profile";
-import Auth from "./pages/Auth/Auth";
+import Chats from "./pages/Chats/Chats";
+import Notifications from "./pages/Notifications/Notifications";
+import LikedPosts from "./pages/LikedPosts/LikedPosts";
+import Github from "./pages/Github/Github";
 
 function App() {
   return (
     <div className="overflow-hidden text-[#fafafa] p-4">
+      {/* <Auth /> */}
       {/* <Home /> */}
       <Profile />
-      {/* <Auth /> */}
+      {/* <Chats /> */}
+      {/* <Notifications /> */}
+      {/* <LikedPosts /> */}
+      {/* <Github /> */}
     </div>
   );
 }
--- src/pages/GithubRepo/GithubRepo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const GithubRepo = () => {
-  return (
-    <div>GithubRepo</div>
-  )
-}
-
-export default GithubRepo
