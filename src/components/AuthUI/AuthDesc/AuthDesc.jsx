2a9727b59ad579d065823898993488321afc95fc
The-Ankit-Maurya <ankitmaurya20223@gmail.com>
2024-07-25 18:31:44 +0530

    changed some code in AuthDesc.jsx

--- src/components/AuthUI/AuthDesc/AuthDesc.jsx
+++ src/components/AuthUI/AuthDesc/AuthDesc.jsx
@@ -1,15 +1,20 @@
-import "./AuthDesc.css";
 import React from "react";
 import Logo from "../../../assets/logo.png";
 import Apple from "../../../assets/apple.png";
 import Arrow from "../../../assets/arrow.png";
 import About from "../../../assets/about.jpg";
-import Image1 from "../../../assets/post1.jpg";
-import Image2 from "../../../assets/post2.jpg";
-import Image3 from "../../../assets/post3.jpg";
-import Image4 from "../../../assets/post4.jpg";
-import Image5 from "../../../assets/post5.jpg";
-import Image6 from "../../../assets/post6.jpg";
+import Image1 from "../../../assets/post67.jpg";
+import Image2 from "../../../assets/post68.jpg";
+import Image3 from "../../../assets/post75.jpg";
+import Image4 from "../../../assets/post82.jpg";
+import Image5 from "../../../assets/post45.jpg";
+import Image6 from "../../../assets/post55.jpg";
+import Image7 from "../../../assets/post57.jpg";
+import Image8 from "../../../assets/post63.jpg";
+import Image9 from "../../../assets/post64.jpg";
+import Image10 from "../../../assets/post38.jpg";
+import Image11 from "../../../assets/post40.jpg";
+import Image12 from "../../../assets/post20.jpg";
 import Google from "../../../assets/google.png";
 import Windows from "../../../assets/windows.png";
 import Twitter from "../../../assets/twitter.png";
@@ -76,37 +81,67 @@ const AuthDesc = () => {
             alt="Gallery Image 6"
             className="w-custom h-auto block rounded-[18px]"
           />
+          <img
+            src={Image7}
+            alt="Gallery Image 7"
+            className="w-custom h-auto block rounded-[18px]"
+          />
+          <img
+            src={Image8}
+            alt="Gallery Image 8"
+            className="w-custom h-auto block rounded-[18px]"
+          />
+          <img
+            src={Image9}
+            alt="Gallery Image 9"
+            className="w-custom h-auto block rounded-[18px]"
+          />
+          <img
+            src={Image10}
+            alt="Gallery Image 10"
+            className="w-custom h-auto block rounded-[18px]"
+          />
+          <img
+            src={Image11}
+            alt="Gallery Image 11"
+            className="w-custom h-auto block rounded-[18px]"
+          />
+          <img
+            src={Image12}
+            alt="Gallery Image 12"
+            className="w-custom h-auto block rounded-[18px]"
+          />
         </div>
-        <div className="login top-1/2 left-1/2 rounded-lg absolute bg-[#1e1e1e] flex items-center justify-center">
-          <div className="bg-[#1e1e1e] p-[40px] rounded-lg text-center text-white">
-            <div className="w-1/4 ml-[105px] flex self-center justify-center bg-black rounded-[14px] mb-4">
-              <img src={Logo} alt="" className="w-[45px] h-[45px] m-[10px]" />
+        <div className="top-1/2 left-1/2 rounded-lg absolute flex items-center justify-center w-3/4 h-3/4 -translate-x-1/2 -translate-y-1/2">
+          <div className="rounded-lg text-center text-white w-1/2">
+            <div className="flex items-center justify-center">
+              <div className="w-1/4 flex items-center justify-center bg-[#282828] rounded-[14px] mb-4">
+                <img src={Logo} alt="" className="w-11 h-11 m-[10px]" />
+              </div>
             </div>
-            <h2 className="mb-[20px] text-[24px]">Welcome Back</h2>
-            <p className="mb-[20px] text-[#aaa]">
-              Don't have an account yet?
-              <a href="#" className="text-[#1a73e8] ml-1">
-                Sign up
-              </a>
-            </p>
-            <form className="mb-[20px]">
-              <div className="mb-[20px] mt-4">
+            <h2 className="mb-[20px] w- text-[24px]">Welcome Back</h2>
+            <form className="mb-5">
+              <div className="mb-5 mt-4">
                 <input
                   type="email"
                   id="email"
                   name="email"
                   placeholder="Email Address"
-                  className="w-full p-[10px] rounded bg-[#2e2e2e] text-white"
+                  className="w-full p-[10px] rounded outline-none bg-[#282828] text-white"
                 />
               </div>
-              <div className="mb-[20px]">
+              <div className="mb-5">
                 <input
                   type="password"
                   id="password"
                   name="password"
                   placeholder="Password"
-                  className="w-full p-[10px] rounded bg-[#2e2e2e] text-white"
+                  className="w-full p-[10px] rounded outline-none bg-[#282828] text-white"
                 />
+              </div>
+              <div className="mt-5 mb-5 flex items-center justify-between">
+                <span className="text-[#282828] text-[15px]">Remember Me</span>
+                <input type="checkbox" className="h-5 w-5 cursor-pointer" />
               </div>
               <button
                 type="submit"
@@ -115,27 +150,27 @@ const AuthDesc = () => {
                 Login
               </button>
             </form>
-            <div className="or-divider flex items-center text-center my-[20px] mx-[0px] text-[#aaa]">
+            <div className="before:flex-1 after:flex-1 before:border-t-[3px] after:border-t-[3px] before:border-[#52b788] after:border-[#52b788] before:mr-2.5 after:ml-2.5 flex items-center text-center my-[20px] mx-[0px] text-[#52b788]">
               <span>OR</span>
             </div>
             <div className="flex justify-between">
-              <button className="apple-login bg-[#333] border-none rounded p-[10px] cursor-pointer">
+              <button className="apple-login bg-[#282828] border-none rounded p-[10px] cursor-pointer">
                 <img src={Apple} alt="Apple Logo" className="w-5 h-5" />
               </button>
-              <button className="google-login bg-[#333] border-none rounded p-[10px] cursor-pointer">
+              <button className="google-login bg-[#282828] border-none rounded p-[10px] cursor-pointer">
                 <img src={Google} alt="Google Logo" className="w-5 h-5" />
               </button>
-              <button className="windows-login bg-[#333] border-none rounded p-[10px] cursor-pointer">
+              <button className="windows-login bg-[#282828] border-none rounded p-[10px] cursor-pointer">
                 <img src={Windows} alt="Windows Logo" className="w-5 h-5" />
               </button>
-              <button className="facebook-login bg-[#333] border-none rounded p-[10px] cursor-pointer">
+              <button className="facebook-login bg-[#282828] border-none rounded p-[10px] cursor-pointer">
                 <img src={Facebook} alt="Facebook Logo" className="w-5 h-5" />
               </button>
-              <button className="twitter-login bg-[#333] border-none rounded p-[10px] cursor-pointer">
-                <img src={Twitter} alt="Twitter Logo" className="w-5 h-5" />
-              </button>
-              <button className="linkedin-login bg-[#333] border-none rounded p-[10px] cursor-pointer">
+              <button className="linkedin-login bg-[#282828] border-none rounded p-[10px] cursor-pointer">
                 <img src={LinkedIn} alt="linkedIn Logo" className="w-5 h-5" />
+              </button>
+              <button className="twitter-login bg-[#282828] border-none rounded p-[10px] cursor-pointer">
+                <img src={Twitter} alt="Twitter Logo" className="w-5 h-5" />
               </button>
             </div>
           </div>
